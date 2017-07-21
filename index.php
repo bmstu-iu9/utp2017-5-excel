e3195068f416d304bb7f4c221bb03ac452f2b7a8
Andrey Smirnov <kupihleba@yandex.ru>
2017-07-21 16:07:25 +0300

    View: Initial design
    Initial design to start with

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta content="Browser excel tables">
+		
+        <title>Excel</title>
+		
+		<link rel="stylesheet" href="CSS/Style.css">
+		<link rel="stylesheet" href="Fonts/fonts.css"
+    </head>
+	<hidden id="csrf-token"></hidden>
+    <body>
+        <h1 class="middle" id="header">utp2017-5-excel</h1>
+		<div class="content">
+			<div class="panel">
+				f(x) = <input id="in">
+			</div>
+			<table class="table">
+				<tr>
+					<td>Kind</td>
+					<td>of</td>
+					<td>table</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				</tr>
+				<tr>
+					<td>some</td>
+					<td>text</td>
+					<td>here</td>
+				</tr>
+			</table>
+		</div>
+		<footer>
+            Contact us +7(123)000-00-00
+        </footer>
+    </body>
+
+</html>
