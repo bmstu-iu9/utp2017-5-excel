913287b63e7d91298e9d9699f9931389144c2e89
SimpleCreations <befv@mail.ru>
2017-07-30 02:20:20 +0300

    Базовая стурктура проекта

---
+++ app/ui.js
@@ -0,0 +1,13 @@
+if(typeof window !== "object" || window === null || typeof document !== "object" || document === null) {
+    throw new Error("A window with a document is required");
+}
+
+const ui = {
+
+    attach(spreadsheet) {
+
+
+
+    }
+
+};
