d3cdf54411b96c23d306567d4a47acaf6f5c7659
Andrey Smirnov <kupihleba@yandex.ru>
2017-08-25 22:31:40 +0300

    Усовершенствование импорта таблиц
    Добавлена поддержка строк SharedStrings

--- app/xlsx-manager.js
+++ app/xlsx-manager.js
@@ -62,17 +62,18 @@ const XLSXManager = class {
             let sharedStrings = new XLSXManager._SharedStrings();
             sheet.fillData(this._cells);
 
-            zip.folder("_rels").file(".rels", XLSXManager._Samples.rels);
+            zip.folder("_rels").file(".rels", XLSXManager._Samples.RELS);
             zip.folder("xl").folder("worksheets").file("sheet1.xml", sheet.toString());
-            zip.folder("xl/worksheets/_rels").file("sheet1.xml.rels", XLSXManager._Samples.sheetRels);
-            zip.folder("xl/_rels").file("workbook.xml.rels", XLSXManager._Samples.workbookRels);
-            zip.file("xl/styles.xml", XLSXManager._Samples.styles);
+            zip.folder("xl/worksheets/_rels").file("sheet1.xml.rels", XLSXManager._Samples.SHEET_RELS);
+            zip.folder("xl/_rels").file("workbook.xml.rels", XLSXManager._Samples.WORKBOOK_RELS);
+            zip.file("xl/styles.xml", XLSXManager._Samples.STYLES);
             zip.file("xl/sharedStrings.xml", sharedStrings.toString());
-            zip.file("xl/workbook.xml", XLSXManager._Samples.workbook);
-            zip.folder("xl/drawings").file("drawing1.xml", XLSXManager._Samples.drawing);
-            zip.file("[Content_Types].xml", XLSXManager._Samples.contentInfo);
+            zip.file("xl/workbook.xml", XLSXManager._Samples.WORKBOOK);
+            zip.folder("xl/drawings").file("drawing1.xml", XLSXManager._Samples.DRAWING);
+            zip.file("[Content_Types].xml", XLSXManager._Samples.CONTENT_INFO);
             return zip.generateAsync({type: type});
         }
+
 
         /**
          * Loads the content to the attached spreadsheet
@@ -80,30 +81,49 @@ const XLSXManager = class {
          */
         fill(blob) {
             JSZip.loadAsync(blob).then((zip) => {
-                let cells = zip.files["xl/worksheets/sheet1.xml"].async("string").then((data) => {
-                    return (new window.DOMParser()).parseFromString(data, "text/xml")
+                let cells = zip.files["xl/worksheets/sheet1.xml"].async("string").then((data) => { // The cell data is here
+                    return (new DOMParser()).parseFromString(data, "text/xml")
                         .getElementsByTagName("sheetData")[0].childNodes;
                 });
-                cells.then((arr) => {
+                let strings = zip.file("xl/sharedStrings.xml").async("string").then((str) => { // Strings are stored here
+                    return (new DOMParser()).parseFromString(str, "text/xml")
+                        .getElementsByTagName("t")
+                });
+
+                Promise.all([cells, strings]).then((args) => {
+                    const arr = args[0];
+                    const strings = Array.from(args[1]).map(elem => elem.textContent);
                     let rowsCount = arr.length; // REQUIRE HEIGHT -> automatically
                     let colCount = 0;
                     arr.forEach((row) => {
                         const cs = row.getElementsByTagName("c");
+
                         if (colCount < cs.length) {
-                            colCount = cs.length; // REQUIRE WIDTH -> automatically
-                        }
+                            colCount = cs.length;
+                        } // REQUIRE WIDTH -> automatically
                         Array.from(cs).forEach(obj => {
                             let addr = this._getIndexes(obj.getAttribute("r"));
-                            if (obj.getElementsByTagName("f")[0]) {
-                                console.log("formula:", obj.getElementsByTagName("f")[0].textContent);
-                                this._spreadsheet.setFormula(addr.row, addr.column, obj.getElementsByTagName("f")[0].textContent);
-                            } else if (obj.getElementsByTagName("v")[0]) {
-                                console.log("textContent:", obj.getElementsByTagName("v")[0].textContent);
-                                this._spreadsheet.setFormula(obj.getElementsByTagName("v")[0].textContent)
+                            switch (obj.getAttribute("t")) {
+                                case "s": {
+                                    if (obj.getElementsByTagName("v")[0]) {
+                                        this._spreadsheet.setFormula(addr.row, addr.column, `"${strings[obj.getElementsByTagName("v")[0].textContent]}"`); // No char escape here !!! TODO
+                                    }
+                                    break;
+                                }
+                                default: {
+                                    if (obj.getElementsByTagName("f")[0]) {
+                                        //console.log("formula:", obj.getElementsByTagName("f")[0].textContent);
+                                        this._spreadsheet.setFormula(addr.row, addr.column, obj.getElementsByTagName("f")[0].textContent);
+                                    } else if (obj.getElementsByTagName("v")[0]) {
+                                        //console.log("textContent:", obj.getElementsByTagName("v")[0].textContent);
+                                        this._spreadsheet.setFormula(addr.row, addr.column, obj.getElementsByTagName("v")[0].textContent);
+                                    } else
+                                        console.error("Unknown object", obj);
+                                }
                             }
                         });
                     });
-                })
+                });
             });
         }
 
@@ -279,12 +299,12 @@ XLSXManager._Sheet = class {
  */
 XLSXManager._Samples = class {
 };
-XLSXManager._Samples.rels = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/></Relationships>';
-XLSXManager._Samples.workbookRels = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/><Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/></Relationships>';
+XLSXManager._Samples.RELS = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/></Relationships>';
+XLSXManager._Samples.WORKBOOK_RELS = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/><Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/></Relationships>';
 
-XLSXManager._Samples.drawing = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<xdr:wsDr xmlns:xdr="http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing" xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:dgm="http://schemas.openxmlformats.org/drawingml/2006/diagram"/>';
+XLSXManager._Samples.DRAWING = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<xdr:wsDr xmlns:xdr="http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing" xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:dgm="http://schemas.openxmlformats.org/drawingml/2006/diagram"/>';
 
-XLSXManager._Samples.styles = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"><fonts count="2"><font><sz val="10.0"/><color rgb="FF000000"/><name val="Arial"/></font><font/></fonts><fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="lightGray"/></fill></fills><borders count="1"><border><left/><right/><top/><bottom/></border></borders><cellStyleXfs count="1"><xf borderId="0" fillId="0" fontId="0" numFmtId="0" applyAlignment="1" applyFont="1"/></cellStyleXfs><cellXfs count="2"><xf borderId="0" fillId="0" fontId="0" numFmtId="0" xfId="0" applyAlignment="1" applyFont="1"><alignment/></xf><xf borderId="0" fillId="0" fontId="1" numFmtId="0" xfId="0" applyAlignment="1" applyFont="1"><alignment/></xf></cellXfs><cellStyles count="1"><cellStyle xfId="0" name="Normal" builtinId="0"/></cellStyles><dxfs count="0"/></styleSheet>';
-XLSXManager._Samples.workbook = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mx="http://schemas.microsoft.com/office/mac/excel/2008/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:mv="urn:schemas-microsoft-com:mac:vml" xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:xm="http://schemas.microsoft.com/office/excel/2006/main"><workbookPr/><sheets><sheet state="visible" name="Sheet1" sheetId="1" r:id="rId3"/></sheets><definedNames/><calcPr/></workbook>';
-XLSXManager._Samples.sheetRels = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/drawing" Target="../drawings/drawing1.xml"/></Relationships>';
-XLSXManager._Samples.contentInfo = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default ContentType="application/xml" Extension="xml"/><Default ContentType="application/vnd.openxmlformats-package.relationships+xml" Extension="rels"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml" PartName="/xl/worksheets/sheet1.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml" PartName="/xl/sharedStrings.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.drawing+xml" PartName="/xl/drawings/drawing1.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml" PartName="/xl/styles.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml" PartName="/xl/workbook.xml"/></Types>';
+XLSXManager._Samples.STYLES = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"><fonts count="2"><font><sz val="10.0"/><color rgb="FF000000"/><name val="Arial"/></font><font/></fonts><fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="lightGray"/></fill></fills><borders count="1"><border><left/><right/><top/><bottom/></border></borders><cellStyleXfs count="1"><xf borderId="0" fillId="0" fontId="0" numFmtId="0" applyAlignment="1" applyFont="1"/></cellStyleXfs><cellXfs count="2"><xf borderId="0" fillId="0" fontId="0" numFmtId="0" xfId="0" applyAlignment="1" applyFont="1"><alignment/></xf><xf borderId="0" fillId="0" fontId="1" numFmtId="0" xfId="0" applyAlignment="1" applyFont="1"><alignment/></xf></cellXfs><cellStyles count="1"><cellStyle xfId="0" name="Normal" builtinId="0"/></cellStyles><dxfs count="0"/></styleSheet>';
+XLSXManager._Samples.WORKBOOK = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mx="http://schemas.microsoft.com/office/mac/excel/2008/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:mv="urn:schemas-microsoft-com:mac:vml" xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:xm="http://schemas.microsoft.com/office/excel/2006/main"><workbookPr/><sheets><sheet state="visible" name="Sheet1" sheetId="1" r:id="rId3"/></sheets><definedNames/><calcPr/></workbook>';
+XLSXManager._Samples.SHEET_RELS = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/drawing" Target="../drawings/drawing1.xml"/></Relationships>';
+XLSXManager._Samples.CONTENT_INFO = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + String.fromCharCode(13) + '<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default ContentType="application/xml" Extension="xml"/><Default ContentType="application/vnd.openxmlformats-package.relationships+xml" Extension="rels"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml" PartName="/xl/worksheets/sheet1.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml" PartName="/xl/sharedStrings.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.drawing+xml" PartName="/xl/drawings/drawing1.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml" PartName="/xl/styles.xml"/><Override ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml" PartName="/xl/workbook.xml"/></Types>';
